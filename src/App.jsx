494dcf80973d2269d974ca40f16dee150bb88f47
RukmalR <rukmalravindu99@gmail.com>
2025-10-15 20:39:29 +0530

    Remove input field from App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div >App
-        <input type="text" className="border-2 border-blue-500" />
+    <div >
+      
     </div>
   )
 }
 
 export default App
+
